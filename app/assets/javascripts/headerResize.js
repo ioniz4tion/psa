5b64aa83affe0c1f455cb104b60c4442cfedcbb4
iqover9000 <iqover9000@gmail.com>
2016-02-08 01:22:58 -0800

    fixed animation and captions

--- app/assets/javascripts/headerResize.js
+++ app/assets/javascripts/headerResize.js
@@ -6,9 +6,16 @@ $(document).ready(function() {
 
     var setHeaderHeight = function() {
         var heightSource = $(window).height() - 170;
+        var heightSource2;
         $header.css('height', heightSource);
-        $canvasHolder.css('width', heightSource);
-        console.log("hello");
+
+        if ($(window).width() >= 1200) {
+            heightSource2 = $(window).height() - 170;
+        } else {
+            heightSource2 = $(window).height() - 281;
+        }
+
+        $canvasHolder.css('width', heightSource2);
     };
 
     $(window).resize(function() {
